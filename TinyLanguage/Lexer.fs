c31fc648cee56e16aec964902d0e741c2f6f6abb
Craig Stuntz <craigstuntz@gmail.com>
2015-05-05 16:33:23 -0400

    Remove implementations

--- TinyLanguage/Lexer.fs
+++ TinyLanguage/Lexer.fs
@@ -7,30 +7,5 @@ type Lexeme =
     | LiteralInt   of int
     | Unrecognized of char
 
-let private isIdentifierStart (c: char) =
-    System.Char.IsLetter c || System.Char.IsPunctuation c || System.Char.IsSymbol c
-let private isIdentifierBody (c: char) =
-    System.Char.IsLetter c || System.Char.IsPunctuation c || System.Char.IsSymbol c || System.Char.IsDigit c
-
-let rec private lexChars (source: char list) : Lexeme list =
-    match source with 
-    | '(' :: rest -> LeftParenthesis  :: lexChars rest
-    | ')' :: rest -> RightParenthesis :: lexChars rest
-    | c   :: rest when isIdentifierStart c        -> lexName (source, "")
-    | d   :: rest when System.Char.IsDigit d      -> lexNumber(source, "")
-    | []          -> []
-    | w   :: rest when System.Char.IsWhiteSpace w -> lexChars rest
-    | c   :: rest -> Unrecognized c :: lexChars rest
-and lexName(source: char list, name: string) = 
-    match source with 
-    | c :: rest when isIdentifierStart c && name = "" -> lexName(rest, name + c.ToString())
-    | c :: rest when isIdentifierBody c               -> lexName(rest, name + c.ToString())
-    | _ -> Identifier(name) :: lexChars(source)
-and lexNumber (source: char list, number: string) = 
-    match source with
-    | d :: rest when System.Char.IsDigit(d) ->
-        lexNumber (rest, number + d.ToString())
-    | rest -> LiteralInt(System.Int32.Parse(number)) :: lexChars rest
-
 let lex (source: string): Lexeme list =
-    lexChars (List.ofSeq source)
+    []
--- TinyLanguage/Parser.fs
+++ TinyLanguage/Parser.fs
@@ -32,46 +32,5 @@ type private ParseState = {
 let private error (state : ParseState, message: string): ParseState =
     { state with Expressions = state.Expressions @ [ Error message ] }
 
-let rec private parseExpression (state : ParseState): ParseState =
-    match state.Remaining with
-    | LeftParenthesis     :: Identifier name    :: rest -> parseInvoke (name, { state with Remaining = rest })
-    | LeftParenthesis     :: wrong -> error (state, sprintf "%A cannot follow '('." wrong) 
-    | RightParenthesis    :: _     -> error (state, "Unmatched )")
-    | Identifier   name   :: _     -> error (state, sprintf "Unrecognized identifier '%s'." name) 
-    | LiteralInt   number :: rest  ->  
-        { Expressions = state.Expressions @ [ ConstantInt number ]; Remaining = rest }
-    | Unrecognized char   :: _    -> error (state, sprintf "Unexpected character %A" char )
-    | [] -> state
-and private parseInvoke (identifier: string, state : ParseState) =
-    let arguments = parseArguments { state with Expressions = [] }
-    match parseOperation identifier with
-    | Some operation -> 
-        parseExpression { Expressions = state.Expressions @ [ Invoke(Builtin operation, arguments.Expressions) ]; Remaining = arguments.Remaining }
-    | None -> error (state, sprintf "Unknown function '%s'." identifier) 
-and private parseArguments (state : ParseState) : ParseState =
-    match state.Remaining with 
-    | [] -> error (state, "')' expected.")
-    | RightParenthesis :: rest -> { state with Remaining = rest }
-    | _ -> parseArguments (parseExpression state)
-
-let rec private parseExpressions (state : ParseState): ParseState =
-    let parsed = parseExpression state
-    match parsed.Remaining with
-    | [] -> parsed
-    | _  -> parseExpressions parsed
-
-let rec private containsMain expressions = 
-    match expressions with 
-    | Defun ("main", _) :: _-> true
-    | [] -> false
-    | _ :: rest -> containsMain rest
-
-let private ensureHasMainFunction = function
-    | [] -> []
-    | expressions when containsMain expressions -> expressions
-    | [ expr ]    -> [ Defun("main", [ expr ]) ]
-    | expressions -> expressions @ [ Error "No main function found." ]
-
 let parse (lexemes: Lexeme list): Expression list=
-    let parsed = parseExpressions { Expressions = []; Remaining = lexemes }
-    parsed.Expressions |> ensureHasMainFunction
+    []
